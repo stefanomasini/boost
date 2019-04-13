2da0820388d05cdcbbef8342338a87c49bf41bf1
Stefano Masini <stefano@stefanomasini.com>
2019-04-13 22:20:53 +0200

    shaft encoder with gray code

---
+++ src/boost/sensors/sensors_adapter.py
@@ -0,0 +1,47 @@
+from RPi import GPIO
+
+
+class GPIOSensorsAdapter(object):
+    def __init__(self, sensor_pins, bouncetime, on_edge_callback):
+        self.sensor_pins = sensor_pins
+        self.sensors_mapping = {}
+        for device, pins in sensor_pins.items():
+            for sensor_idx, sensor_pin in enumerate(pins):
+                self.sensors_mapping[sensor_pin] = (device, sensor_idx)
+        self.bouncetime = bouncetime
+        self.on_edge_callback = on_edge_callback
+        self.previous_sensor_values = {}
+
+    def read_all_sensor_values(self):
+        return dict((device, ''.join([str(GPIO.input(sensor_pin)) for sensor_pin in sensor_pins])) for device, sensor_pins in self.sensor_pins.items())
+
+    def start(self):
+        GPIO.setmode(GPIO.BCM)
+        for sensor_pin in self.sensors_mapping.keys():
+            GPIO.setup(sensor_pin, GPIO.IN)
+            GPIO.add_event_detect(sensor_pin, GPIO.BOTH, callback=self._on_edge, bouncetime=self.bouncetime)
+
+    def stop(self):
+        GPIO.cleanup()
+
+    def _on_edge(self, sensor_pin):
+        current_value = str(GPIO.input(sensor_pin))
+        if current_value != self.previous_sensor_values:
+            self.on_edge_callback(*self.sensors_mapping[sensor_pin], current_value)
+        self.previous_sensor_values = [current_value]
+
+
+if __name__ == '__main__':
+    from time import sleep
+    def callback(device, sensor_idx, value):
+        print('Sensor {0} {1} {2}'.format(device, sensor_idx, value))
+    adapter = GPIOSensorsAdapter({'A': [11, 9]}, 20, callback)
+    adapter.start()
+    for _device, _value in adapter.read_all_sensor_values():
+        print('Current value, {0}: {1}'.format(_device, _value))
+    try:
+        while True:
+            sleep(1)
+    except:
+        pass
+    adapter.stop()
---
+++ src/boost/sensors/shaft_encoder.py
@@ -0,0 +1,49 @@
+from .sensors_adapter import GPIOSensorsAdapter
+from ..gray_code import generate_gray_codes
+
+
+class ShaftEncoders(object):
+    def __init__(self, num_bits_per_device, sensor_pins_msb_first, bouncetime, on_device_angle_changed):
+        self.sensor_pins_msb = sensor_pins_msb_first
+        self._on_device_angle_changed = on_device_angle_changed
+        for device, sensor_pins in sensor_pins_msb_first.items():
+            assert len(sensor_pins) == num_bits_per_device, \
+                'Device {0} has wrong number of sensor_pins ({1} instead of {2})'.format(device, len(sensor_pins), num_bits_per_device)
+        all_codes = generate_gray_codes(num_bits_per_device)
+        self.gray_code_to_integer_map = dict((code, idx) for idx, code in enumerate(all_codes))
+        self.num_codes = len(all_codes)
+        self.adapter = GPIOSensorsAdapter(sensor_pins_msb_first, bouncetime, self._on_sensor_value)
+        self.current_values = {}
+
+    def start(self):
+        self.adapter.start()
+        self.current_values = self.adapter.read_all_sensor_values()
+        return dict((device, self._code_to_angle(code)) for device, code in self.current_values.items())
+
+    def stop(self):
+        self.adapter.stop()
+
+    def _on_sensor_value(self, device, bit_position, value):
+        self.current_values[device][bit_position] = value
+        self._on_device_angle_changed(device, self._code_to_angle(self.current_values[device]))
+
+    def _code_to_angle(self, code):
+        value = self.gray_code_to_integer_map[code]
+        return 360.0 / self.num_codes * value
+
+
+if __name__ == '__main__':
+    from time import sleep
+
+    def callback(device, angle):
+        print('Device {0} {1}'.format(device, angle))
+    shaft_encoders = ShaftEncoders(2, {'A': [11, 9]}, 20, callback)
+    current_values = shaft_encoders.start()
+    for _device, _angle in current_values.items():
+        print('Current value, {0}: {1}'.format(_device, _angle))
+    try:
+        while True:
+            sleep(1)
+    except:
+        pass
+    shaft_encoders.stop()
