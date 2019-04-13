d5a2bdec12d84939aa70b12861ed9c3319e7579f
Stefano Masini <stefano@stefanomasini.com>
2019-04-13 18:39:18 +0200

    sensors test code

--- src/boost/sensors/shaft_sensors.py
+++ src/boost/sensors/shaft_sensors.py
@@ -1,27 +1,39 @@
 from RPi import GPIO
 from time import sleep
 
+GPIO.setmode(GPIO.BCM)
+
 
 def main():
-    GPIO.setmode(GPIO.BCM)
-
     left_sensor = 11
     right_sensor = 9
 
     GPIO.setup(left_sensor, GPIO.IN)
     GPIO.setup(right_sensor, GPIO.IN)
 
+    def my_callback(channel):
+        print("falling edge detected on left")
+
+    def my_callback2(channel):
+        print("rising edge detected on left")
+
+    GPIO.add_event_detect(left_sensor, GPIO.FALLING, callback=my_callback, bouncetime=300)
+
+    GPIO.add_event_detect(left_sensor, GPIO.RISING, callback=my_callback2, bouncetime=300)
+
     try:
         while True:
-            if not GPIO.input(left_sensor):
-                print("Robot is straying off to the right, move left captain!")
-            elif not GPIO.input(right_sensor):
-                print("Robot is straying off to the left, move right captain!")
-            else:
-                print("Following the line!")
-            sleep(0.2)
+            # if not GPIO.input(left_sensor):
+            #     print("Robot is straying off to the right, move left captain!")
+            # elif not GPIO.input(right_sensor):
+            #     print("Robot is straying off to the left, move right captain!")
+            # else:
+            #     print("Following the line!")
+            sleep(1)
     except:
-        GPIO.cleanup()
+        pass
+
+    GPIO.cleanup()
 
 
 if __name__ == '__main__':
