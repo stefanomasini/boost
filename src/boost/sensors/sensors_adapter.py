683df41a15bb16c299dafd2ac0ef3ce53967ef2b
Stefano Masini <stefano@stefanomasini.com>
2019-04-16 09:15:48 +0200

    shaft encoder: calculate speed

--- src/boost/sensors/sensors_adapter.py
+++ src/boost/sensors/sensors_adapter.py
@@ -3,36 +3,38 @@ import time
 
 
 class GPIOSensorsAdapter(object):
-    def __init__(self, sensor_pins, read_delay, on_edge_callback):
+    def __init__(self, sensor_pins, read_delay):
         self.sensor_pins = sensor_pins
-        self.sensors_mapping = {}
-        for device, pins in sensor_pins.items():
-            for sensor_idx, sensor_pin in enumerate(pins):
-                self.sensors_mapping[sensor_pin] = (device, sensor_idx)
+        self.sensor_indexes = dict([(sensor_pin, idx) for idx, sensor_pin in enumerate(sensor_pins)])
         self.read_delay = read_delay
+        self.on_edge_callback = None
+        self.sensor_values = None
+
+    def start(self, on_edge_callback):
         self.on_edge_callback = on_edge_callback
-        self.previous_sensor_values = {}
-
-    def read_all_sensor_values(self):
-        return dict((device, [self._read_sensor(sensor_pin) for sensor_pin in sensor_pins]) for device, sensor_pins in self.sensor_pins.items())
-
-    def start(self):
         GPIO.setmode(GPIO.BCM)
-        for sensor_pin in self.sensors_mapping.keys():
+        for sensor_pin in self.sensor_pins:
             GPIO.setup(sensor_pin, GPIO.IN)
+        self.sensor_values = [self._read_sensor(sensor_pin) for sensor_pin in self.sensor_pins]
+        for sensor_pin in self.sensor_pins:
             GPIO.add_event_detect(sensor_pin, GPIO.BOTH, callback=self._on_edge)
+        return self.sensor_values
 
     def stop(self):
         GPIO.cleanup()
 
     def _on_edge(self, sensor_pin):
+        if not self.sensor_values:
+            return
         # Wait for a small delay before reading the sensor, as it seems to be more reliable
         # Source: https://raspberrypi.stackexchange.com/a/49692
         time.sleep(self.read_delay / 1000.0)
         current_value = self._read_sensor(sensor_pin)
-        if current_value != self.previous_sensor_values.get(sensor_pin, None):
-            self.on_edge_callback(*self.sensors_mapping[sensor_pin], current_value)
-        self.previous_sensor_values[sensor_pin] = current_value
+        sensor_pin_idx = self.sensor_indexes[sensor_pin]
+        call_callback = current_value != self.sensor_values[sensor_pin_idx]
+        self.sensor_values[sensor_pin_idx] = current_value
+        if call_callback:
+            self.on_edge_callback(self.sensor_values)
 
     def _read_sensor(self, sensor_pin):
         # The sensor returns a high value (1) for black, and a low value (0) for white, which is what we want
@@ -41,12 +43,11 @@ class GPIOSensorsAdapter(object):
 
 
 if __name__ == '__main__':
-    def callback(device, sensor_idx, value):
-        print('Sensor {0} {1} {2}'.format(device, sensor_idx, value))
-    adapter = GPIOSensorsAdapter({'A': [9, 11]}, 5, callback)
-    adapter.start()
-    for _device, _value in adapter.read_all_sensor_values().items():
-        print('Current value, {0}: {1}'.format(_device, _value))
+    def print_values(sensor_values):
+        print('Sensors: {0}'.format(''.join([('.' if v == '1' else 'O') for v in sensor_values])))
+    adapter = GPIOSensorsAdapter([11, 9, 10, 22, 27], 5)
+    initial_values = adapter.start(print_values)
+    print_values(initial_values)
     try:
         while True:
             time.sleep(1)
--- src/boost/sensors/shaft_encoder.py
+++ src/boost/sensors/shaft_encoder.py
@@ -1,49 +1,110 @@
-from .sensors_adapter import GPIOSensorsAdapter
+from collections import namedtuple
+from datetime import timedelta
 from ..gray_code import generate_gray_codes
 
 
+WheelPosition = namedtuple('WheelPosition', 'position angle code')
+
+
 class ShaftEncoders(object):
-    def __init__(self, num_bits_per_device, sensor_pins_msb_first, bouncetime, on_device_angle_changed):
-        self.sensor_pins_msb = sensor_pins_msb_first
-        self._on_device_angle_changed = on_device_angle_changed
-        for device, sensor_pins in sensor_pins_msb_first.items():
-            assert len(sensor_pins) == num_bits_per_device, \
-                'Device {0} has wrong number of sensor_pins ({1} instead of {2})'.format(device, len(sensor_pins), num_bits_per_device)
+    def __init__(self, sensors_adapter, devices, clock, stasis_timeout, max_speed, log_error_message):
+        self.sensors_adapter = sensors_adapter
+        self.devices = devices
+        self.clock = clock
+        self.stasis_timeout = timedelta(seconds=stasis_timeout)
+        self.max_speed = max_speed  # Degrees per second
+        self.log_error_message = log_error_message
+
+        self._on_device_angle_changed = None
+        self.current_positions = None
+        self.last_position_ts = {}
+
+        # Initialize gray code tables
+        num_bits_per_device = len(list(devices.values())[0])
         all_codes = generate_gray_codes(num_bits_per_device)
         self.gray_code_to_integer_map = dict((code, idx) for idx, code in enumerate(all_codes))
         self.num_codes = len(all_codes)
-        self.adapter = GPIOSensorsAdapter(sensor_pins_msb_first, bouncetime, self._on_sensor_value)
-        self.current_values = {}
 
-    def start(self):
-        self.adapter.start()
-        self.current_values = self.adapter.read_all_sensor_values()
-        return dict((device, self._code_to_angle(code)) for device, code in self.current_values.items())
+    def start(self, on_device_angle_changed):
+        self._on_device_angle_changed = on_device_angle_changed
+        current_sensor_values = self.sensors_adapter.start(self._on_sensor_values)
+        self.current_positions = self._decode_values(current_sensor_values)
+        now = self.clock.now()
+        for device in self.current_positions.keys():
+            self.last_position_ts[device] = now
+        return self.current_positions
 
     def stop(self):
-        self.adapter.stop()
+        self.sensors_adapter.stop()
 
-    def _on_sensor_value(self, device, bit_position, value):
-        self.current_values[device][bit_position] = value
-        self._on_device_angle_changed(device, self._code_to_angle(self.current_values[device]))
+    def _on_sensor_values(self, values):
+        positions = {}
+        speeds = {}
+        now = self.clock.now()
+        for device, position in self._decode_values(values).items():
+            last_position = self.current_positions[device]
+            if position != last_position:
+                positions[device] = position
+                last_position_ts = self.last_position_ts.get(device, None)
+                speed = None
+                if last_position_ts:
+                    elapsed_time_since_last_read = now - last_position_ts
+                    if elapsed_time_since_last_read < self.stasis_timeout:
+                        speed = self._calculate_angular_speed(last_position, position, elapsed_time_since_last_read)
+                if speed and speed > self.max_speed:
+                    self.log_error_message('Speed too high ({0} deg/sec). Probably the result of a faulty sensor read on the shaft encoder.'.format(speed))
+                    return
+                speeds[device] = speed
+            self.last_position_ts[device] = now
+            self.current_positions[device] = position
+        if len(positions) > 0:
+            self._on_device_angle_changed(positions, speeds)
 
-    def _code_to_angle(self, code):
-        value = self.gray_code_to_integer_map[''.join(code)]
-        return 360.0 / self.num_codes * value
+    def _decode_values(self, values):
+        result = {}
+        for device, sensor_indexes in self.devices.items():
+            sensor_values = [values[idx] for idx in sensor_indexes]
+            position = self._code_to_position(sensor_values)
+            result[device] = position
+        return result
+
+    def _code_to_position(self, code):
+        code_str = ''.join(code)
+        value = self.gray_code_to_integer_map[code_str]
+        angle = 360.0 / self.num_codes * value
+        return WheelPosition(value, angle, code_str)
+
+    def _calculate_angular_speed(self, old_position, new_position, elapsed_time):
+        angle_diff_a = new_position.angle - old_position.angle
+        angle_diff_b = old_position.angle - new_position.angle
+        if angle_diff_a < 0:
+            angle_diff_a += 360.0
+        if angle_diff_b < 0:
+            angle_diff_b += 360.0
+        # Choose the minimum of the two angles
+        minimum_angle_diff = min(angle_diff_a, angle_diff_b)
+        return minimum_angle_diff / elapsed_time.total_seconds()  # Degrees per second
 
 
 if __name__ == '__main__':
-    from time import sleep
+    def main():
+        from .sensors_adapter import GPIOSensorsAdapter
+        from time import sleep
+        from ..clock import Clock
 
-    def callback(device, angle):
-        print('Device {0} {1}'.format(device, angle))
-    shaft_encoders = ShaftEncoders(5, {'A': [11, 9, 10, 22, 27]}, 5, callback)  # Missing LSB: 17
-    current_values = shaft_encoders.start()
-    for _device, _angle in current_values.items():
-        print('Current value, {0}: {1}'.format(_device, _angle))
-    try:
-        while True:
-            sleep(1)
-    except:
-        pass
-    shaft_encoders.stop()
+        clock = Clock()
+
+        adapter = GPIOSensorsAdapter([11, 9, 10, 22, 27], 5)  # Missing LSB 17
+
+        def print_values(positions, speeds=None):
+            print('Positions: {0} Speeds: {1}'.format(positions, speeds))
+        shaft_encoders = ShaftEncoders(adapter, {'A': [0, 1, 2, 3, 4]}, clock, 1.0, 800, print)
+        current_values = shaft_encoders.start(print_values)
+        print_values(current_values)
+        try:
+            while True:
+                sleep(1)
+        except:
+            pass
+        shaft_encoders.stop()
+    main()
