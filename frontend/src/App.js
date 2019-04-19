e92c74698b3d72960d2bdb56965a5ed0bf88e0a3
Stefano Masini <stefano@stefanomasini.com>
2019-04-19 23:23:46 +0200

    commented out unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,7 +38,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 // import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 // import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
+// import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 // import AssignmentIcon from '@material-ui/icons/Assignment';
 import Slider from '@material-ui/lab/Slider';
